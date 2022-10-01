4c75e526aab8427eec2b7c7a051752237dc760b9
Frank <frank@inter-zone.de>
2022-10-01 15:23:47 +0200

    cleartoken

--- .cleartoken.py
+++ .cleartoken.py
@@ -1,10 +1,24 @@
-# Read in the file
-with open('helloworld.py', 'r') as file :
-  filedata = file.read()
 
-# Replace the target string
-filedata = filedata.replace('12345', 'abcde')
+# # Read in the file
+# with open('helloworld.py', 'r') as file :
+#   filedata = file.read()
 
-# Write the file out again
-with open('helloworld.py', 'w') as file:
-  file.write(filedata)
+# # Replace the target string
+# filedata = filedata.replace('12345', 'abcde')
+
+# # Write the file out again
+# with open('helloworld.py', 'w') as file:
+#   file.write(filedata)
+
+#with open(file)
+
+import fileinput
+
+filename="helloworld.py"
+
+# print("hallo")
+
+for line in fileinput.input(filename, inplace=True):
+    if "TOKEN=" in line:
+      print("TOKEN=YOUR_TOKEN_HERE")
+      #print("TOKEN=YOUR_TOKEN_HERE", line, end='')
--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,1 @@
-TOKEN=abcde
-print(TOKEN)
+TOKEN=12345
